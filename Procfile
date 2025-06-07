dda79d45f8ab310a2a4eb8e785e28659dc41df27
matias-cisnero <matiascisnero2014@gmail.com>
2025-06-07 12:03:45 -0300

    arreglo de estructura y archivos para railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python autoencoder-api.py
--- autoencoder-api.py
+++ autoencoder-api.py
@@ -26,5 +26,6 @@ def predict_api():
     result = predict(x)
     return jsonify({'prediction': result})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
